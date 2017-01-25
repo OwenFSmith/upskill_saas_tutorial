d5434095ec48f4346e3cb9080045a3b972102aba
Owen Smith <smithfowen@gmail.com>
2017-01-25 05:18:42 +0000

    Added contact form mailer file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
- get 'contact-us', to: 'contacts#new'
- resources :contacts
+ get 'contact-us', to: 'contacts#new', as: 'new_contact'
+ resources :contacts, only: :create
 end
